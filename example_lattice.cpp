49847e1a3ab25064ac53e8e7c3e89bbac3e4c825
Kevin Ryczko <kryczko@viz1-uoit.sharcnet.ca>
2013-05-10 13:23:11 -0400

    added fcc lattice

--- example_lattice.cpp
+++ example_lattice.cpp
@@ -27,14 +27,17 @@ ofstream out;
 out.open(outputfile.c_str());
 
 
-out << 2*rows*columns*stacks  << "\n\n";
 
-if (type == "bcc")
-{
+
 
 double c0_x = -2.0, c0_y = -2.0, c0_z = -2.0, c1_x, c1_y, c1_z = -1.0;
 
 double x0_values[columns], y0_values[rows], x1_values[columns], y1_values[rows], z0_values[stacks], z1_values[stacks];
+
+if (type == "bcc")
+{
+
+out << 2*rows*columns*stacks << "\n\n";
 
 for (int j = 0; j < stacks; j ++)
 {
@@ -59,8 +62,46 @@ for (int i = 0; i < columns; i ++)
 		y0_values[n] = c0_y;
 		y1_values[n] = c1_y;
 		out << 	"C\t" << x0_values[i] << "\t" << y0_values[n] << "\t" << z0_values[j] << endl;
+	
 		out << "C\t" << x1_values[i] << "\t" << y1_values[n] << "\t" << z1_values[j]  << endl;
+			
 	}
+}
+}
+}
+if (type == "fcc")
+{
+
+out << 4*rows*columns*stacks << "\n\n";
+
+for (int j = 0; j < stacks; j ++)
+{
+        c0_z += 2.0;
+        c1_z += 2.0;
+
+        z0_values[j] = c0_z;
+        z1_values[j] = c1_z;
+
+        c0_x = -2.0;
+for (int i = 0; i < columns; i ++)
+{
+        c0_x += 2.0;
+        c1_x = c0_x + 1.0;
+        x0_values[i] = c0_x;
+        x1_values[i] = c1_x;
+
+        c0_y = -2.0;
+        for (int n = 0; n < rows; n ++)
+        {
+                c0_y += 2.0;
+                c1_y = c0_y + 1.0;
+                y0_values[n] = c0_y;
+                y1_values[n] = c1_y;
+                out <<  "C\t" << x0_values[i] << "\t" << y0_values[n] << "\t" << z0_values[j] << endl;
+                out << "C\t" << x1_values[i] << "\t" << y1_values[n] << "\t" << z0_values[j]  << endl;
+                out << "C\t" << x0_values[i] << "\t" << y1_values[n] << "\t" << z1_values[j] << endl;
+		out << "C\t" << x1_values[i] << "\t" << y0_values[n] << "\t" << z1_values[j] << endl; 
+        }
 }
 }
 }
